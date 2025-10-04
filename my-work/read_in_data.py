93028c85e9afeacfe055e7ede6ea7c7e49c918ab
elainecazetta <elaine.rodrigues1@gmail.com>
2025-10-04 15:17:06 +0100

    lab topic 2

--- my-work/read_in_data.py
+++ my-work/read_in_data.py
@@ -7,14 +7,28 @@ import csv
 FILENAME= "data.csv"
 DATADIR = "../my-work/"
 
+# to calculate the average age:
+# Convert the string that is read into an integer:
+
+#with open (DATADIR + FILENAME, "rt") as fp:
+    #reader = csv.reader(fp, delimiter=",")
+    #linecount = 0
+    #total = 0
+    #for line in reader:
+        #if not linecount: # first row is header row
+            #pass
+    #else: # all subsequent rows
+        #total += int(line[1])
+    #linecount += 1
+#print (f"average is {total/(linecount-1)}")
+
+# Using DictReader():
 with open (DATADIR + FILENAME, "rt") as fp:
-    reader = csv.reader(fp, delimiter=",")
-    linecount = 0
+    reader = csv.DictReader(fp, delimiter="," , quoting=csv.QUOTE_NONNUMERIC)
     total = 0
+    count = 0
     for line in reader:
-        if not linecount: # first row is header row
-            pass
-    else: # all subsequent rows
-        total += int(line[1])
-    linecount += 1
-print (f"average is {total/(linecount-1)}")
+        total += line['age']
+        #print (line)
+        count +=1 
+print (f"average is {total/(count)}")
