4dbbddef4b837f4723268e0d0923a634322bdc83
elainecazetta <elaine.rodrigues1@gmail.com>
2025-10-04 16:13:02 +0100

    assignment topic 2

--- assignments/assignment02-bankholidays.py
+++ assignments/assignment02-bankholidays.py
@@ -1,11 +1,34 @@
 # This program prints out the dates of the bank holidays that happen in northern Ireland.
 # Author: Elaine Cazetta
 
+# import requests
+
+# url ="https://www.gov.uk/bank-holidays.json"
+# response = requests.get(url)
+# data = response.json()
+
+#for event in data['northern-ireland']['events']:
+    #print(event['date'], "-", event['title'])
+
+# Extra marks:
+# Modify the program to print the bank holidays that are unique to northern Ireland:
+
 import requests
 
 url ="https://www.gov.uk/bank-holidays.json"
 response = requests.get(url)
 data = response.json()
 
-for event in data['northern-ireland']['events']:
-    print(event['date'], "-", event['title'])
+# Get titles for England & Wales and Scotland:
+england_titles = {event['title'] for event in data['england-and-wales']['events']}
+scotland_titles = {event['title'] for event in data['scotland']['events']}
+
+# Northern Ireland events:
+ni_events = data['northern-ireland']['events']
+
+print("Bank holidays unique to Northern Ireland:")
+for event in ni_events:
+    title = event['title']
+    date = event['date']
+    if title not in england_titles and title not in scotland_titles:
+        print(f"{date} - {title}")
