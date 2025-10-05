422a2d266a29b8bc06e01727108bbb97863ed94f
Elaine Rodrigues Cazetta <elaine.rodrigues1@gmail.com>
2025-10-05 14:33:33 +0000

    minor update

--- assignments/assignment02-bankholidays.py
+++ assignments/assignment02-bankholidays.py
@@ -1,5 +1,6 @@
-# This program prints out the dates of the bank holidays that happen in northern Ireland.
 # Author: Elaine Cazetta
+
+# This program prints out the dates of the bank holidays that happen in northern Ireland:
 
 # import requests
 
